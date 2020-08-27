e6a67da6ec308e8090c92f8b9781d41b4501efc7
Thomas Roller <thomasroller@yahoo.ca>
2020-08-27 13:52:50 -0400

    Add graphing program

---
+++ scripts/Cell-DEVS_co2-charting/Constants.py
@@ -0,0 +1,18 @@
+# Carleton University (ARSLab)
+# Thomas Roller
+
+# Class: Constants
+# Purpose: a central location for constants
+# Arguments:
+#     none
+class Constants:
+
+    # Class: Graph
+    # Purpose: graph constants
+    # Arguments:
+    #     none
+    class Graph:
+
+        labelX = "time (steps)"
+        labelY = "concentration (ppm)"
+        title = None  # set to None for Graph.getTitle to create a title
